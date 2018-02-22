2d6473aefc98e37f8a3f0623f366ae6425bfe09d
Fabrício Ribeiro <fabricio@fabriciolribeiro.com>
2018-02-22 17:53:22 -0300

    Classe ArquivoDeLog melhorada.

--- orgbkp.py
+++ orgbkp.py
@@ -6,12 +6,14 @@ import lzma
 import smtplib
 import os
 import boto3
-import time as t
+import os.path
+from boto3 import exceptions
 from configparser import ConfigParser
 from email.mime.text import MIMEText
 from fdb import services
 
 __author__ = 'Fabricio L. Ribeiro'
+__version__ = '0.1'
 
 ''' Aplicação destinada a fazer backup da base de dados Firebird da Orgsystem
     Software, testar o restore e, se tudo estiver dentro dos conformes, faz
@@ -24,18 +26,12 @@ __author__ = 'Fabricio L. Ribeiro'
     a fim de parametrizar os dados particulares da execução.
 '''
 
-''' TODO: Criar classes para gerenciar log e conexão ao Firebird. '''
 
-log = ''
-var = dict()
-con = ''
-
-
-class Conf(object):
-    def __init__(self, ini):
+class Conf:
+    def __init__(self, arqini):
         self.__conf = ConfigParser()
-        self.__ini = ini
-        self.__conf.read(ini)
+        self.__ini = arqini
+        self.__conf.read(arqini)
         self.__dconf = dict(self.__conf['GERAL'])
         self.__dawsconf = dict(self.__conf['AWS'])
         self.__dmailconf = dict(self.__conf['E-mail'])
@@ -49,11 +45,18 @@ class Conf(object):
     def mail(self, param):
         return self.__dmailconf[param]
 
+    def set_conf(self, param, valor):
+        if (param is not None) and (param != '') and (valor is not None) and (valor != ''):
+            self.__dconf[param] = valor
+            return True
+        else:
+            return False
+
     def __str__(self):
         return "Caminho do arquivo de inicialização: " + self.__ini
 
 
-class ArquivoDeLog(object):
+class ArquivoDeLog:
     def __init__(self, ini):
 
         con = ConexaoFB(ini.geral('ip_servidor'), ini.geral('user_fb'), ini.geral('pass_fb'),
@@ -61,9 +64,9 @@ class ArquivoDeLog(object):
                         self)
 
         self.__log = '\n\n|=====================< ' + now() + ' >=====================|\n' \
-                     'Versão do Firebird: ' + con.versao_firebird() + '\n' \
-                     'Usuario do banco de dados: ' + ini.geral('user_fb') + '\n' \
-                     'IP do servidor: ' + ini.geral('ip_servidor') + '\n'
+            'Versão do Firebird: ' + con.versao_firebird() + '\n' \
+            'Usuario do banco de dados: ' + ini.geral('user_fb') + \
+            '\nIP do servidor: ' + ini.geral('ip_servidor') + '\n'
 
         self.registra_log('Processo iniciado.')
 
@@ -89,8 +92,8 @@ class ArquivoDeLog(object):
                 self.registra_log(msg)
 
 
-class ConexaoFB(object):
-    __con, __cliente, __bd, __bkp, __cliente, arqlog = '', '', '', '', '', ''
+class ConexaoFB:
+    __con, __cliente, __bd, __bkp, arqlog = '', '', '', '', ''
 
     def __init__(self, ip, usuario, senha, uribd, uribkp, cliente,
                  arqlog: ArquivoDeLog):
@@ -113,7 +116,7 @@ class ConexaoFB(object):
     def backup(self):
         self.arqlog.registra_log('Backup iniciado.')
         try:
-            self.__con.backup(self.__bd, self.__bkp, metadata_only=True, collect_garbage=False)
+            self.__con.backup(self.__bd, self.__bkp, metadata_only=False, collect_garbage=False)
             self.__con.wait()
         except fdb.fbcore.DatabaseError as erro:
             msg = 'Houve um problema no backup: ' + str(erro)
@@ -125,7 +128,7 @@ class ConexaoFB(object):
     def restore(self):
         self.arqlog.registra_log('Restore iniciado.')
         try:
-            self.__con.restore(self.__bkp, self.__bkp + 'teste.fdb', replace=1)
+            self.__con.restore(self.__bkp, 'teste.fdb', replace=1)
             self.__con.wait()
         except fdb.fbcore.DatabaseError as erro:
             msg = 'Houve um problema no restore: ' + str(erro)
@@ -138,7 +141,7 @@ class ConexaoFB(object):
         return self.__con.get_server_version()
 
 
-def nome_arquivo_datahora(ini):
+def nome_arquivo_final(ini):
     return ini.geral('nome_cliente').replace(' ', '') + now('nomearq')
 
 
@@ -155,124 +158,106 @@ def now(opc=''):
     return escolhas.get(opc, "")
 
 
-def compacta_bkp():
-    global var
+def compacta_bkp(ini: Conf, arqlog: ArquivoDeLog):
+    # global var
     # subprocess.call(['7z', 'a', arq_7zip, arq_backup])
-    arq_backup = var['dir_backup'] + 'osbd.fbk'
-    arq_7zip = var['dir_backup'] + nome_arquivo_datahora()
-    var['arq_7zip'] = nome_arquivo_datahora()
+    arq_backup = ini.geral('dir_backup') + 'osbd.fbk'
+    arq_7zip = ini.geral('dir_backup') + nome_arquivo_final(ini)
+    ini.set_conf('arq_7zip', nome_arquivo_final(ini))
+    if not os.path.isfile(arq_backup):
+        arqlog.registra_log('Arquivo de backup não foi encontrado para compactação.')
+        return False
     arq_handler = open(arq_backup, 'rb')
-    registra_log('Iniciando compactação.')
+    arqlog.registra_log('Iniciando compactação.')
     with lzma.open(arq_7zip, "wb") as arqz:
         arqz.write(arq_handler.read())
-    registra_log('Compactação concluída.')
+    if os.path.isfile(arq_7zip):
+        arqlog.registra_log('Compactação concluída.')
+        return True
+    else:
+        arqlog.registra_log('Houve algum problema na compactação do arquivo de backup.')
+        return False
 
 
-def upload_to_s3(ini):
+def envia_s3(ini: Conf, alog: ArquivoDeLog):
     session = boto3.Session(aws_access_key_id=ini.aws('aws_access_key_id'),
                             aws_secret_access_key=ini.aws('aws_secret_access_key'))
-    print(session.get_available_resources())
-    print(session.get_available_services())
-    # s3 = session.resource("s3")
 
+    s3 = session.resource("s3")
 
-    s3 = session.client("s3")
-    resposta = s3.list_buckets()
-    print("resposta: {}".format(resposta))
-    buckets = [bucket['Name'] for bucket in resposta['Buckets']]
-    print("lista de buckets: {}".format(buckets))
-    s3.create_bucket(Bucket='bucket-teste-flr')
-    acl = s3.get_bucket_acl(Bucket='bucket-teste-flr')
-    print(acl)
+    # TODO futuramente usar access key do próprio cliente pra subir o backup
+    arq_zip_path = ini.geral('dir_backup') + ini.geral('arq_7zip')
+    if not os.path.isfile(arq_zip_path):
+        alog.registra_log('Arquivo de backup não foi compactado.')
+        return False
+    dados = open(arq_zip_path, 'rb')
+    caminho = ini.geral('nome_cliente') + '/' + now('ano') + '/' + now('mes') + '/' + now('dia') \
+        + '/' + ini.geral('arq_7zip')
 
-    # dados = open(var['dir_backup'] + var['arq_7zip'], 'rb')
-    # caminho = var['nome_cliente'] + '/' + now('ano') + '/' + now('mes') + '/' + now('dia') + '/' + var['arq_7zip']
-    # try:
-    #     s3.Bucket('orgsystem.backup').put_object(Key=caminho, Body=dados)
-    #     registra_log('Upload para S3 concluído.')
-    # except boto3.S3UploadFailedError as err:
-    #     msg = 'Houve um problema no restore: ' + str(err)
-    #     print(msg)
-    #     registra_log(msg)
-
-
-def notifica_email():
-    global log
-    global var
-
-    msg = MIMEText(log)
-    msg['Subject'] = 'OrgBKP: Notificação para %s' % var['nome_cliente']
-    msg['From'] = var['mail_from']
-    msg['To'] = var['mail_to']
     try:
-        s = smtplib.SMTP(var['mail_server'] + ':' + var['mail_port'])
+        alog.registra_log('Iniciando upload para S3.')
+        s3.Bucket('bucket-teste-flr').put_object(Key=caminho, Body=dados)
+        alog.registra_log('Upload para S3 concluído.')
+    except exceptions.S3UploadFailedError as err:
+        msg = 'Houve um problema no restore: ' + str(err)
+        print(msg)
+        alog.registra_log(msg)
+
+    return True
+
+
+def notifica_email(ini: Conf, alog: ArquivoDeLog):
+    msg = MIMEText(alog.mostra_log())
+    msg['Subject'] = 'orgbkp: Notificação para %s' % ini.geral('nome_cliente')
+    msg['From'] = ini.mail('mail_from')
+    msg['To'] = ini.mail('mail_to')
+
+    try:
+        s = smtplib.SMTP(ini.mail('mail_server') + ':' + ini.mail('mail_port'))
         s.starttls()
-        s.login(var['mail_user'], var['mail_pass'])
+        s.login(ini.mail('mail_user'), ini.mail('mail_pass'))
         s.send_message(msg)
         s.quit()
     except smtplib.SMTPException as e:
         msg = 'Não foi possível enviar o e-mail de notificação. Favor verificar configurações de e-mail:' + str(e)
         print(msg)
-        registra_log(msg)
+        alog.registra_log(msg)
         exit()
-    registra_log('Notificação enviada para: ' + var['mail_to'] + '.')
-
-
-class Teste:
-    def __init__(self, alog: ArquivoDeLog):
-        self.arqlog = alog
-
-    def escreve_no_log(self, texto):
-        self.arqlog.registra_log(texto)
+    alog.registra_log('Notificação enviada para: ' + ini.mail('mail_to') + '.')
 
 
 def main(argv):
-    global log
-    global var
-
-    # print('Argumentos:', str(argv))
-
-    # instanciar parser config (ini)
-
-    # instanciar ArquivoDeLog
-
-    ### A inicialização do arquivo de log passou a ser feita no construtor
-    ###    da classe ArquivoDeLog.
-    # log = log + '\n\n|=====================< ' + now('ano') + ' >=====================|\n'
-    # log = log + 'Versão do Firebird: ' + conecta_firebird().get_server_version() + '\n'
-    # log = log + 'Usuario do banco de dados: ' + var['user_fb'] + '\n'
-    # log = log + 'IP do servidor: ' + var['ip_servidor'] + '\n'
-    # registra_log('Processo iniciado.')
-
-    rel_backup = backup()
-    rel_restore = restore()
-
-    compacta_bkp()
-
-    upload_to_s3()
-
-    notifica_email()
-
-    gera_arq_log()
-
-
-def test(argv):
-    # conf, awsconf, mailconf = parseconfig()
-    ini = Conf('orgbkp.ini')
-    print(ini.geral('ip_servidor'))
+    arqconfig = 'orgbkp.ini'
+    if len(argv) > 1:
+        if argv[1] == '--config' or argv[1] == '-c':
+            if argv[2] is not None:
+                arqconfig = argv[2]
+    ini = Conf(arqconfig)
     alog = ArquivoDeLog(ini)
-    print(alog.mostra_log())
-    alog.registra_log("Teste.")
-    teste_log = Teste(alog)
-    teste_log.escreve_no_log("Esse veio da classe Teste.")
-    alog.registra_log("Outra mensagem.")
-    alog.registra_log("Última.")
+    fb = ConexaoFB(
+        ini.geral('ip_servidor'),
+        ini.geral('user_fb'),
+        ini.geral('pass_fb'),
+        ini.geral('dir_bd'),
+        ini.geral('dir_backup'),
+        ini.geral('nome_cliente'),
+        alog
+    )
+    fb.backup()
+    fb.restore()
+    s7zip = compacta_bkp(ini, alog)
+    if not s7zip:
+        alog.registra_log('Não foi possível compactar o arquivo.')
+    else:
+        alog.registra_log('Compactação do arquivo de backup: OK.')
+    s3 = envia_s3(ini, alog)
+    if not s3:
+        alog.registra_log('Não foi possível enviar o backup para o S3.')
+    else:
+        alog.registra_log('Upload do backup para S3: OK.')
+    notifica_email(ini, alog)
     alog.grava_arq_log()
-    print(nome_arquivo_datahora(ini))
-    # Testar conexão FB, métodos: backup e restore.
-    upload_to_s3(ini)
 
 
 if __name__ == "__main__":
-    ## main(sys.argv)
-    test(sys.argv)
+    main(sys.argv)
