36775725da7675dfcda4384ccb590724cd7716e7
Fabrício Ribeiro <fabricio@fabriciolribeiro.com>
2018-02-27 18:04:38 -0300

    Novos testes com argparse.

--- orgbkp/parseargs.py
+++ orgbkp/parseargs.py
@@ -3,7 +3,16 @@ import argparse as ap
 
 
 def main(argv):
-    pass
+    parser = ap.ArgumentParser(description='Apenas um teste.',
+                               epilog='Desnvolvido por Fabricio L. Ribeiro '
+                                      '(contato@fabriciolribeiro.com)')
+    parser.add_argument('inteiro', metavar='N', type=int, nargs='+',
+                        help='um inteiro para o acumulador')
+    parser.add_argument('--sum', dest='accumulate', action='store_const',
+                        const=sum, default=max, help='soma os inteiros (padrão: acha '
+                                                     'o max)')
+    args = parser.parse_args()
+    print(args.accumulate(args.inteiro))
 
 
 if __name__ == '__main__':
