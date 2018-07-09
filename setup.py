6b67bb61260945e788a2db6fb6f29931cfdfe0f2
Fabrĩcio Ribeiro <fabricio@fabriciolribeiro.com>
2018-07-09 09:17:44 -0300

    Alteração no nome do projeto.

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup
 setup(
     name='OrgBKP',
     version='0.0.1',
-    packages=['orgbkp'],
-    url='https://github.com/flribeiro1983/OrgBKP',
+    packages=['cldbkp'],
+    url='https://github.com/flribeiro/CloudDataBackup',
     license='MIT',
     author='Fabrício L. Ribeiro',
     author_email='fabricio@fabriciolribeiro.com',
-    description='Programa para automação do backup das bases de dados de clientes da Org.',
+    description='Programa para automação do backup de bancos de dados em cloud storages.',
     install_requires=['boto3',
                       'fdb'],
     python_requires='>=3',
     package_data={
-        '': ['orgbkp_model.ini'],
+        '': ['cldbkp_model.ini'],
         }
 )
